5a1c0a8254993a2e1225abc2d4a2630f7a0733f1
Yokomon <marowmars@gmail.com>
2019-10-28 00:30:04 +0100

    I have initialized the constructor function in order to access my state object

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component{
+    // super is a reference to the parents constructor function
+    constructor(props){
+        super(props);
+    }
+    // Always call the render function whenever we want to show JSX on the root document
     render (){
         window.navigator.geolocation.getCurrentPosition(
             (position) => console.log(position),
