94c50b92dd17f788c0911d3bac6d2bb5b7cacd50
Yokomon <marowmars@gmail.com>
2019-10-27 17:53:21 +0100

    Successfully added the location api for the users location

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-class App extends React.Component{
-   render(){
-       return <h1>This is where the document takes place!</h1>
-   }
+const App = () => {
+    window.navigator.geolocation.getCurrentPosition(
+        (position) => console.log(position),
+        (err) => console.log(err)
+
+    )
+    return <h1>Hi there</h1>
 }
 
 ReactDOM.render(<App />, document.querySelector("#root"));
