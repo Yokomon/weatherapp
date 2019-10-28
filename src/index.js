72463d9c66a6455eb1ad9418a9dde37c0edea11b
Yokomon <marowmars@gmail.com>
2019-10-28 01:13:07 +0100

    I added an error message which can be updated from the callback Api and it is updated from the setState function

--- src/index.js
+++ src/index.js
@@ -7,19 +7,29 @@ class App extends React.Component{
     constructor(props){
         super(props);
 
-        this.state ={lat: null, err: ""};
+        this.state ={lat: null, errMessage: ""};
 
         window.navigator.geolocation.getCurrentPosition(
             position => {
                 // to update the state object, we called setState function
             this.setState({lat: position.coords.latitude});
-            }
+            },
+            err => this.setState({
+                errMessage: err.message
+            })
     
         )
     }
     // Always call the render function whenever we want to show JSX on the root document
     render (){
-        return <h1>Latitude: {this.state.lat} </h1>
+        return (
+            <div>
+                  <h1>Latitude: {this.state.lat} </h1>
+            <br/>
+            <h3>Error: {this.state.errMessage}</h3>
+            </div>
+          
+            )
     }
 }
 
