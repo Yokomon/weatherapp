0b01d2ee6fe70e07c174eb267020ecd0c184fe28
Yokomon <marowmars@gmail.com>
2019-10-28 03:06:09 +0100

    added logic for the icon set from semantic ui

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -11,8 +11,13 @@ const getSeason = (lat,month) => {
 const SeasonDiplay = props => {
     const season = getSeason(props.lat, new Date().getMonth())
     const weatherText = season === 'winter' ? "It's gonna rain today, go out with an umbrella" : "It's a sunny weather over there";
-
-    return <h1>{weatherText}</h1>
+    const icon = season === 'winter' ? 'snowflake' : 'sun'
+    return (
+    <div>
+        <i className = {`${icon} icon`} />
+        <h1>{weatherText}</h1> 
+        <i className = {`${icon} icon`} />
+    </div>)
 }
 
 export default SeasonDiplay;
